6d6e364b114c307db827989728b2f6d9a4c9d43b
kyww <kyww@DESKTOP-9CT8NK2>
2024-08-03 00:33:18 +0900

    spring boot setting

---
+++ run_us_server/src/main/java/com/server/runus/RunUsServerApplication.java
@@ -0,0 +1,13 @@
+package com.server.runus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunUsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunUsServerApplication.class, args);
+	}
+
+}
---
+++ run_us_server/src/test/java/com/server/runus/RunUsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.runus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunUsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
