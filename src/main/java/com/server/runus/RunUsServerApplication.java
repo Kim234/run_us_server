fd035f9edc6f433cb35920e43427e74fa6e80e72
Kim234 <djehoaos@naver.com>
2024-08-14 22:59:58 +0900

    test cd

--- src/main/java/com/server/runus/RunUsServerApplication.java
+++ src/main/java/com/server/runus/RunUsServerApplication.java
@@ -1,8 +1,9 @@
 package com.server.runus;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
+@SpringBootApplication
 public class RunUsServerApplication {
 
 	public static void main(String[] args) {
