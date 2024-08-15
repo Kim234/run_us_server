4a76f7f41c0af7eee6d76692616b6e8d87ab377b
Kim234 <djehoaos@naver.com>
2024-08-15 23:52:59 +0900

    fix application 6

--- src/main/java/com/server/runus/RunUsServerApplication.java
+++ src/main/java/com/server/runus/RunUsServerApplication.java
@@ -2,11 +2,10 @@ package com.server.runus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //@SpringBootTest
 @SpringBootApplication
-public class RunUsServerApplication extends SpringBootServletInitializer {
+public class RunUsServerApplication {
 
 	public static void main(String[] args) {
 		
