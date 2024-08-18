9ce7b9a00ed67dc70aa0731bc138dd64cebea5fc
Kim234 <djehoaos@naver.com>
2024-08-18 21:16:29 +0900

    delete initial password

--- src/main/java/com/server/runus/RunUsServerApplication.java
+++ src/main/java/com/server/runus/RunUsServerApplication.java
@@ -2,9 +2,10 @@ package com.server.runus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 //@SpringBootTest
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class RunUsServerApplication {
 
 	public static void main(String[] args) {
